199043e516f3d6a903bf281a5483557f38bf2aa2
anshdholakiya <dholakiyaansh1707@gmail.com>
2024-09-19 15:06:43 +0530

    main ui update

--- Main_app/src/components/Form/NewsForm.jsx
+++ Main_app/src/components/Form/NewsForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useIndianStates, useIndianDistrict } from '../hooks'
+import { useIndianStates, useIndianDistrict } from '../../hooks'
 
 // Form component
 const NewsForm = () => {
